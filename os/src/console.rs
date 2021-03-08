754c407a67d0323dc2713a10508667c9f221ce50
胡亮 <1271446412@qq.com>
2021-03-08 22:36:33 +0800

    finish homework

--- os/src/console.rs
+++ os/src/console.rs
@@ -1,31 +1,106 @@
-use crate::sbi::console_putchar;
-use core::fmt::{self, Write};
+use core::fmt;
+
+use log::{self, Level, LevelFilter, Log, Metadata, Record};
+use core::fmt::Write;
+
+pub fn init() {
+    static LOGGER: SimpleLogger = SimpleLogger;
+    log::set_logger(&LOGGER).unwrap();
+    log::set_max_level(match option_env!("LOG") {
+        Some("ERROR") => LevelFilter::Error,
+        Some("WARN") => LevelFilter::Warn,
+        Some("INFO") => LevelFilter::Info,
+        Some("DEBUG") => LevelFilter::Debug,
+        Some("TRACE") => LevelFilter::Trace,
+        _ => LevelFilter::Info,
+    });
+}
 
 struct Stdout;
 
 impl Write for Stdout {
     fn write_str(&mut self, s: &str) -> fmt::Result {
         for c in s.chars() {
-            console_putchar(c as usize);
+            crate::sbi::console_putchar(c as usize);
         }
         Ok(())
     }
+}
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ({
+        $crate::console::print(format_args!($($arg)*));
+    });
+}
+
+#[macro_export]
+macro_rules! println {
+    ($fmt:expr) => (print!(concat!($fmt, "\n")));
+    ($fmt:expr, $($arg:tt)*) => (print!(concat!($fmt, "\n"), $($arg)*));
+}
+
+/// Add escape sequence to print with color in Linux console
+macro_rules! with_color {
+    ($args: ident, $color_code: ident) => {{
+        format_args!("\u{1B}[{}m{}\u{1B}[0m", $color_code as u8, $args)
+    }};
+}
+
+fn print_in_color(args: fmt::Arguments, color_code: u8) {
+    Stdout.write_fmt(with_color!(args, color_code)).unwrap();
 }
 
 pub fn print(args: fmt::Arguments) {
     Stdout.write_fmt(args).unwrap();
 }
 
-#[macro_export]
-macro_rules! print {
-    ($fmt: literal $(, $($arg: tt)+)?) => {
-        $crate::console::print(format_args!($fmt $(, $($arg)+)?));
+struct SimpleLogger;
+
+impl Log for SimpleLogger {
+    fn enabled(&self, _metadata: &Metadata) -> bool {
+        true
     }
+    fn log(&self, record: &Record) {
+        if !self.enabled(record.metadata()) {
+            return;
+        }
+
+        /*
+        if let Some(tid) = processor().tid_option() {
+            print_in_color(
+                format_args!(
+                    "[{:>5}][{},{}] {}\n",
+                    record.level(),
+                    crate::arch::cpu::id(),
+                    tid,
+                    record.args()
+                ),
+                level_to_color_code(record.level()),
+            );
+        } else {
+            */
+        print_in_color(
+            format_args!(
+                // "[{:>5}][{},-] {}\n",
+                "[{:>5}] {}\n",
+                record.level(),
+                // crate::arch::cpu::id(),
+                record.args()
+            ),
+            level_to_color_code(record.level()),
+        );
+        //}
+    }
+    fn flush(&self) {}
 }
 
-#[macro_export]
-macro_rules! println {
-    ($fmt: literal $(, $($arg: tt)+)?) => {
-        $crate::console::print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
+fn level_to_color_code(level: Level) -> u8 {
+    match level {
+        Level::Error => 31, // Red
+        Level::Warn => 93,  // BrightYellow
+        Level::Info => 34,  // Blue
+        Level::Debug => 32, // Green
+        Level::Trace => 90, // BrightBlack
     }
 }
--- os/src/main.rs
+++ os/src/main.rs
@@ -9,6 +9,8 @@ mod console;
 mod lang_items;
 mod sbi;
 
+use log::{error, warn, info, debug, trace};
+
 global_asm!(include_str!("entry.asm"));
 
 fn clear_bss() {
@@ -34,14 +36,22 @@ pub fn rust_main() -> ! {
         fn boot_stack_top();
     }
     clear_bss();
-    println!("Hello, world!");
-    println!(".text [{:#x}, {:#x})", stext as usize, etext as usize);
-    println!(".rodata [{:#x}, {:#x})", srodata as usize, erodata as usize);
-    println!(".data [{:#x}, {:#x})", sdata as usize, edata as usize);
-    println!(
+    // 初始化 logger
+    crate::console::init();
+
+    error!("Hello, world!");
+    warn!("Hello, world!");
+    info!("Hello, world!");
+    debug!("Hello, world!");
+    trace!("Hello, world!");
+
+    info!(".text [{:#x}, {:#x})", stext as usize, etext as usize);
+    info!(".rodata [{:#x}, {:#x})", srodata as usize, erodata as usize);
+    info!(".data [{:#x}, {:#x})", sdata as usize, edata as usize);
+    info!(
         "boot_stack [{:#x}, {:#x})",
         boot_stack as usize, boot_stack_top as usize
     );
-    println!(".bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
+    info!(".bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
     panic!("Shutdown machine!");
 }
